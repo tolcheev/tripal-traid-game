3314d35cd88b33afd3dccc5370e85efcc50a5b50
Vladislav Tolcheev <tolcheevvladislav@gmail.com>
2022-04-17 16:36:52 +0300

    Lesson 2 up
    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Slider from "./components/Slider";
 import Footer from "./components/Footer";
+import Text from "./components/Text";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Slider />
+        <Text element={'div'}>
+            Тестовый текст блок
+        </Text>
       <Footer />
     </div>
   );
