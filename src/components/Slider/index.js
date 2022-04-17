bf1bf2da6a0345b3927a527de98d1b831a39c2c9
Vladislav Tolcheev <tolcheevvladislav@gmail.com>
2022-04-17 03:53:24 +0300

    lesson-02 домашка к 2 уроку <3

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,21 +1,44 @@
-import s from './Slider.module.css'
+import Container from '../Container';
+import Heading from '../Heading';
+import Text from "../Text";
+import s from './Slider.module.css';
 
 const Slider = () => {
-  return (
-    
-    <section className={s.section}>
-      <div className={s.slider}>
-        <div className={s.container +' '+ s.sliderContent}>
-          <h1 className={s.header}>Wow</h1>
-          <h2 className={s.subheader}>Wow.Wow.Wow</h2>
-          <div className={s.call}>
-            <button className={s.button}>Wow</button>
-          </div>
-        </div>
-      </div>
-    </section>
-    
-  )
+    return (
+        <section className={s.section}>
+            <div className={s.slider}>
+                <Container className={s.sliderContent}>
+
+                    <Heading level={1}>
+                        This is first Header, baby
+                    </Heading>
+
+                    <Heading level={2} black>
+                        This is second Header, damn >3
+                    </Heading>
+
+                    <Text element={'div'} italic>
+                        Тема: «Трагический катарсис: методология и особенности»
+                    </Text>
+
+                    <Text element={'p'} italic={true} disabled>
+                        Прекрасный реферат создан нейроночкой яднуха
+                    </Text>
+
+                    <Text element={'span'}>
+                        Дедуктивный метод индуктивно творит даосизм. Исчисление предикатов, конечно, преобразует примитивный гедонизм. Страсть рассматривается закон исключённого третьего, хотя в официозе принято обратное. Адаптация осмысленно транспонирует примитивный даосизм.
+                        Дистинкция, следовательно, непредвзято индуцирует данный даосизм. Закон внешнего мира, конечно, понимает под собой дуализм. Суждение, по определению, понимает под собой принцип восприятия.
+                        Знак естественно осмысляет бабувизм. Мир решительно порождает и обеспечивает из ряда вон выходящий дуализм, tertium nоn datur. Суждение философски контролирует субъективный гравитационный парадокс.
+                    </Text>
+
+                    <div className={s.call}>
+                        <button className={s.button}>Wow</button>
+                    </div>
+
+                </Container>
+            </div>
+        </section>
+    )
 }
 
 
